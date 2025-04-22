b46c22ab24403d6ea055820c915da310b13d3e2e
MarquiseGT <Marquisegt@gmail.com>
2025-04-22 01:08:13 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import Home from './pages/index.jsx'
 
+console.log("✅ React root mounted")
+
 const container = document.getElementById('root')
 const root = createRoot(container)
-root.render(<Home />)
+
+root.render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>
+)
