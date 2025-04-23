431abc60a6e5e4fd98d64bc75e911b94e49e2d82
MarquiseGT <Marquisegt@gmail.com>
2025-04-23 01:18:50 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/index.jsx'
-import SignalEcho from './pages/Signal.jsx'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Index from './pages/index.jsx'
+import Signal from './pages/signal.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signal" element={<SignalEcho />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
+const App = () => (
+  <Router>
+    <nav style={{ padding: '1rem', backgroundColor: '#f0f0f0' }}>
+      <Link to="/" style={{ marginRight: '1rem' }}>Home</Link>
+      <Link to="/signal">Signal Echo</Link>
+    </nav>
+    <Routes>
+      <Route path="/" element={<Index />} />
+      <Route path="/signal" element={<Signal />} />
+    </Routes>
+  </Router>
 )
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
