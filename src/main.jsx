e627fc706d81edd3bbf6da5e531444d613c972ce
MarquiseGT <Marquisegt@gmail.com>
2025-04-23 12:21:36 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/index.jsx'
 import Chat from './pages/chat.jsx'
+import FieldEcho from './pages/field.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -10,6 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/chat" element={<Chat />} />
+        <Route path="/field" element={<FieldEcho />} />
       </Routes>
     </Router>
   </React.StrictMode>
