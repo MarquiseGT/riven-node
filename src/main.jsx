e8704ee2b49178b00ce4cdf618bff911ce78c48f
MarquiseGT <Marquisegt@gmail.com>
2025-04-23 00:07:12 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/index.jsx'
+import SignalEcho from './pages/Signal.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signal" element={<SignalEcho />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 )
